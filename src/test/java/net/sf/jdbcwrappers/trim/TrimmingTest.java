79badd46d9eee15c0ee3d4f6a5dce054b19ab791
Andreas Veithen <andreas.veithen@gmail.com>
2008-02-26 23:30:36 +0000

    Added test for callable statements.

--- src/test/java/net/sf/jdbcwrappers/trim/TrimmingTest.java
+++ src/test/java/net/sf/jdbcwrappers/trim/TrimmingTest.java
@@ -2,11 +2,15 @@ package net.sf.jdbcwrappers.trim;
 
 import static org.junit.Assert.assertEquals;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.sql.DataSource;
 
@@ -14,6 +18,10 @@ import net.sf.jdbcwrappers.DataSourceWrapper;
 import net.sf.springderby.DeleteDatabaseAction;
 import net.sf.springderby.EmbeddedDataSourceFactory;
 import net.sf.springderby.ExecuteSqlScriptsAction;
+import net.sf.springderby.OnlineAction;
+import net.sf.springderby.proc.DeclareProceduresAction;
+import net.sf.springderby.proc.annotation.DataAccessLevel;
+import net.sf.springderby.proc.annotation.Procedure;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -35,9 +43,18 @@ public class TrimmingTest {
 		factory.setUser("test");
 		factory.setCreate(true);
 		factory.setBeforeStartupAction(new DeleteDatabaseAction());
-		ExecuteSqlScriptsAction action = new ExecuteSqlScriptsAction();
-		action.setScript(new ClassPathResource("/net/sf/jdbcwrappers/trim/trimmingTest.sql"));
-		factory.setAfterCreationAction(action);
+		List<OnlineAction> afterCreationActions = new LinkedList<OnlineAction>();
+		{
+			ExecuteSqlScriptsAction action = new ExecuteSqlScriptsAction();
+			action.setScript(new ClassPathResource("/net/sf/jdbcwrappers/trim/trimmingTest.sql"));
+			afterCreationActions.add(action);
+		}
+		{
+			DeclareProceduresAction action = new DeclareProceduresAction();
+			action.setClassName(TrimmingTest.class.getName());
+			afterCreationActions.add(action);
+		}
+		factory.setAfterCreationActions(afterCreationActions);
 		factory.setAfterShutdownAction(new DeleteDatabaseAction());
 		factory.afterPropertiesSet();
 		dataSource = new DataSourceWrapper(new TrimmingWrapperFactory(), (DataSource)factory.getObject());
@@ -79,6 +96,26 @@ public class TrimmingTest {
 		PreparedStatement statement = connection.prepareStatement("SELECT * FROM TEST WHERE INT_COL=?");
 		try {
 			statement.setInt(1, 12);
+			ResultSet rs = statement.executeQuery();
+			rs.next();
+			assertEquals("test", rs.getString(2));
+			assertEquals("test", rs.getString("CHAR_COL"));
+		}
+		finally {
+			statement.close();
+		}
+	}
+	
+	@Procedure(name="TESTPROC", dataAccessLevel=DataAccessLevel.READS_SQL_DATA)
+	public static void testProc(ResultSet[] resultSet) throws SQLException {
+		Connection connection = DriverManager.getConnection("jdbc:default:connection");
+		resultSet[0] = connection.createStatement().executeQuery("SELECT * FROM TEST");
+	}
+	
+	@Test
+	public void testCallableStatement() throws SQLException {
+		CallableStatement statement = connection.prepareCall("{ call TESTPROC() }");
+		try {
 			ResultSet rs = statement.executeQuery();
 			rs.next();
 			assertEquals("test", rs.getString(2));
